15d3763d151b00a0ec52fbab6dc8a8edf94b7757
AndySave <51698537+AndySave@users.noreply.github.com>
2025-11-05 02:09:09 +0100

    Predict now returns activations for each layer

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import uuid
 import numpy as np
+import nano_nn as nn
 from sklearn.metrics import accuracy_score
 from datasets.adult_income_dataset import AdultIncomeDataset
 from datasets.medical_cost_dataset import MedicalCostDataset
 from loaders import PandasCsvLoader
 from schemas import TrainConfig, AdultIncomeInput, MedicalCostInput, DatasetMetadata
-from nn.builder import SmallModel, MediumModel
+from nn.network import SmallModel, MediumModel, LargeModel
 
 
 app = FastAPI()
@@ -22,14 +23,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-models = {}
+models: dict[str, nn.Module] = {}
 datasets = {
     "adult_income": AdultIncomeDataset(csv_loader=PandasCsvLoader()),
     "medical_cost": MedicalCostDataset(csv_loader=PandasCsvLoader()),
 }
 
 
-@app.post("/api/nn-framework/train/")
+@app.post("/api/nn-framework/train")
 async def train_model(train_config: TrainConfig):
     model_id = str(uuid.uuid4())
     dataset = datasets[train_config.dataset_name]
@@ -38,10 +39,14 @@ async def train_model(train_config: TrainConfig):
     if train_config.model_size == "small":
         model = SmallModel(dataset.num_features(),
                            dataset.num_outputs(), task_type)
-    else:
-        # TODO: Add more models
+    elif train_config.model_size == "medium":
         model = MediumModel(dataset.num_features(),
                             dataset.num_outputs(), task_type)
+    elif train_config.model_size == "large":
+        model = LargeModel(dataset.num_features(),
+                           dataset.num_outputs(), task_type)
+    else:
+        return {"status": "error", "message": f"There is no model with size {train_config.model_size}"}
 
     dataset = datasets[train_config.dataset_name]
 
@@ -53,14 +58,15 @@ async def train_model(train_config: TrainConfig):
         training_loss.append(loss)
 
         if dataset.task_type() != "regression":
-            training_accuracy.append(accuracy_score(dataset.y, (output.squeeze() > 0.5)))
+            training_accuracy.append(accuracy_score(
+                dataset.y, (output.squeeze() > 0.5)))
         model.backward()
 
         print(f'epoch: {i+1}/{train_config.epochs}, loss: {loss:.6f}')
 
     models[model_id] = model
 
-    return {"model_id": model_id, "training_loss": training_loss, "training_accuracy": training_accuracy}
+    return {"model_id": model_id, "training_loss": training_loss, "training_accuracy": training_accuracy if len(training_accuracy) > 0 else None}
 
 
 async def predict(dataset_name: str, input, model_id):
@@ -76,8 +82,31 @@ async def predict(dataset_name: str, input, model_id):
 
     output = float(np.asarray(model.forward(X)).squeeze())
 
+    layer_activations = []
+    temp = X[:]
+    for layer in model.layers:
+        temp = layer.forward(temp)
+
+        # We want the raw output from the last layer
+        if layer is model.layers[-1]:
+            layer_activations.append(temp[0])
+        # TODO: Improve the nn framework so I can use isinstance(layer, nn.Activation) instead
+        elif isinstance(layer, (nn.ReLU, nn.Sigmoid, nn.Softmax)):
+            min_val = np.min(temp)
+            max_val = np.max(temp)
+
+            if max_val > min_val:
+                activation_normalized = (temp - min_val) / (max_val - min_val)
+            else:
+                activation_normalized = np.zeros_like(temp)
+
+            layer_activations.append(activation_normalized[0])
+
+    layer_activations = [a.tolist() for a in layer_activations]
+
     return {
         "output": output,
+        "activations": layer_activations,
     }
 
 
@@ -102,7 +131,8 @@ async def datasets_metadata():
 
 @app.get("/api/nn-framework/networks-metadata")
 async def networks_metadata():
-    metadatas = [SmallModel.metadata(), MediumModel.metadata()]
+    metadatas = [SmallModel.metadata(), MediumModel.metadata(),
+                 LargeModel.metadata()]
     return metadatas
 
 
